0401b71a17e04b49ff5d7382a3e86b409e2c7850
Martin Grunnill <Martin.Grunnill@phac-aspc.gc.ca>
2024-11-20 15:24:47 -0500

    Corrected setup.py.

--- setup.py
+++ setup.py
@@ -31,8 +31,7 @@ setup(
     install_requires=[
         "dark-matter>=1.1.28",
         "pandas>=2.2.2",
-        "python=3.10",
-        "ete3",
-        "six",
+        "ete3>= 3.1.3",
+        "six>=1.16.0",
     ],
 )
